1de74f680a44cea1a6ae3f5032d0a6e3093e6c44
Jaqueline Luiza Leite <jaquelineluiza4444@gmail.com>
2021-06-30 22:24:23 -0300

    Continued to configure application security

--- src/main/java/com/dawii/trabfinal/controllers/PessoaController.java
+++ src/main/java/com/dawii/trabfinal/controllers/PessoaController.java
@@ -1,13 +1,14 @@
 package com.dawii.trabfinal.controllers;
 
 import com.dawii.trabfinal.models.Pessoa;
-import com.dawii.trabfinal.models.request.PessoaRequest;
 import com.dawii.trabfinal.models.response.PessoaResponse;
 import com.dawii.trabfinal.services.IPessoaService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
@@ -39,11 +40,5 @@ public class PessoaController {
         ModelAndView mv = new ModelAndView("usuario/mostrartodos");
         mv.addObject("usuarios", response.getPessoas());
         return mv;
-
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<PessoaResponse> entrar(@RequestBody PessoaRequest request){
-        return ResponseEntity.ok(getService().entrar(request));
     }
 }
--- src/main/java/com/dawii/trabfinal/security/WebSecurityConfig.java
+++ src/main/java/com/dawii/trabfinal/security/WebSecurityConfig.java
@@ -1,46 +1,67 @@
 package com.dawii.trabfinal.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .antMatchers("/vendor/**","/fonts/**","/css/**", "/images/**", "/js/**","/","/api/locacao/abririnserir").permitAll()
+                .antMatchers("/api/usuario/**", "/api/produto/pesquisar", "/api/produto/abrirpesquisa", "/api/produto/remover", "/api/produto/alterar").hasRole("ADMIN")
+                .antMatchers("/api/locacao/abririnserir", "/api/locacao/cadastrar").hasAnyRole("ADMIN", "USUARIO")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                    .loginPage("/login")
                 .permitAll()
-                .and()
+                    .and()
                 .logout()
-                .permitAll();
+                    .logoutSuccessUrl("/");
+    }
 
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select usuario, senha, ativo "
+                        + "from usuario "
+                        + "where usuario = ?")
+                .authoritiesByUsernameQuery("SELECT tab.usuario, papel.nome from"
+                        + "(SELECT usuario.usuario, usuario.codigo from usuario where usuario = ?) as tab "
+                        + " inner join usuario_papel on codigo_usuario = tab.codigo \n"
+                        + " inner join papel on codigo_papel = papel.codigo;")
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public PasswordEncoder passwordEncoder() {
+        String idEncoder = "argon2";
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
 
-        return new InMemoryUserDetailsManager(user);
+        encoders.put("argon2", new Argon2PasswordEncoder());
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+
+        PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idEncoder, encoders);
+
+        return passwordEncoder;
     }
-
 }
